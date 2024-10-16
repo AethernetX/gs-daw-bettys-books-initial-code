df08afc5fd122e349328098334661743fa4e5275
Tam <mthanimr24@gmail.com>
2024-10-16 17:10:38 +0100

    mistakenly commited early

--- create_db.sql
+++ create_db.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS bettys_books;
 USE bettys_books;
 
 # Create the tables
-CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT primary key NOT NULL,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id), publication DATE);
+CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT primary key NOT NULL,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id), publication DATE, stock TINYINT unsigned);
 
 CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT primary key NOT NULL,firstName VARCHAR(23), lastName VARCHAR(23), email VARCHAR(40), username VARCHAR(23), isAdmin BOOL, password VARCHAR(70));
 
