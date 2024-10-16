937f32a0effdd9f95c92a78513c25d3b3dc0aee7
Tam <mthanimr24@gmail.com>
2024-10-16 17:05:00 +0100

    Create DB accomadates for extra information

--- create_db.sql
+++ create_db.sql
@@ -5,7 +5,9 @@ CREATE DATABASE IF NOT EXISTS bettys_books;
 USE bettys_books;
 
 # Create the tables
-CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT primary key NOT NULL,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id), publication DATE);
+
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT primary key NOT NULL,firstName VARCHAR(23), lastName VARCHAR(23), email VARCHAR(40), username VARCHAR(23), isAdmin BOOL, password VARCHAR(70));
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
